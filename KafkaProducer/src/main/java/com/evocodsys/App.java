96760d0dd29a058ed9e4d94b47f3cb6475e52e6a
Zd <zdenek@zdrazil.com>
2023-02-26 22:16:22 +0100

    ver 1

---
+++ KafkaProducer/src/main/java/com/evocodsys/App.java
@@ -0,0 +1,13 @@
+package com.evocodsys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
